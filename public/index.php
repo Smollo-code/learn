d22849c6e291dc7c84598bf7bd29564c98ada2d8
Maximilian Neidel <maximilian.neidel@simovative.com>
2023-10-16 08:55:21 +0200

    Made Calculator

--- public/index.php
+++ public/index.php
@@ -1,9 +1,12 @@
 <?php
-//phpinfo();
+header("Location: main.html");
+exit;
+?>
+phpinfo();
 $neu = 3 / 3.5;
 $test = 'Hallo Welt:' . $neu;
 echo $test;
-$test5 = '6'; //Integer(Int)
+$test5 = 6; //Integer(Int)
 $test6 = 3.14; //Float
 $test7 = True; //Boolean(bool)
 $test8 = 'String'; //String(str)
@@ -17,6 +20,34 @@ if ((!$test10 && $test6 > 1) && $test10 === 1){
 } else {
     echo 'ahhhh';
 }
-$array = [1,2,3,4,'5'];
+$array = [
+    5 => $test8,
+    6 => 'Hallo Welt'
+];
 var_dump($array);
-echo $array[3];
+
+foreach ($array as $key => $value) {
+    continue;
+    echo '<h1>' . $key . $value . '</h1>';
+}
+
+while ($test5 < 30) {
+    $test5++;
+    echo 'Done';
+}
+
+for ($i = 1; $i < 10; $i++) {
+    echo 'Hello';
+}
+
+$i = 0;
+do {
+    echo $i;
+} while ($i > 0);
+
+$test8 = 'world';
+echo match ($test8) {
+    'Hallo' => 'Welt',
+    'Welt' => 'Hallo;',
+    default => 'nix',
+};*/
